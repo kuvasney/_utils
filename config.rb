21c463555b447271085e7e391b6422f96caa0b51
kuvasney <kuvasney@gmail.com>
2016-11-16 11:24:16 -0200

    Add files via upload

---
+++ config.rb
@@ -0,0 +1,33 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+# Syntax
+preferred_syntax = :scss
+
+# Paths
+http_path = "/"
+images_dir = "img"
+javascripts_dir = "js"
+sass_dir = "scss/"
+css_dir = "css/"
+
+if environment == :development
+    line_comments = true
+    output_style = :nested
+    sass_options = {:debug_info => true}
+end
+
+if environment == :production
+    line_comments = false
+    output_style = :compressed
+    sass_options = {:debug_info => false}
+    css_dir = "css/prod"
+
+    require 'fileutils'
+        on_stylesheet_saved do |file|
+            if File.exists?(file)
+            filename = File.basename(file, File.extname(file))
+            #File.rename(file, "css" + "/" + filename + ".min" + File.extname(file))
+        end
+    end
+end 
